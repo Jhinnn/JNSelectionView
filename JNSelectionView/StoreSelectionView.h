0dcdb6de217c4dfa1399201d933045d4c3b51e82
pengpengduduguagua <2979468570@qq.com>
2019-03-13 15:22:18 +0800

    first commit

---
+++ JNSelectionView/StoreSelectionView.h
@@ -0,0 +1,13 @@
+//
+//  StoreSelectionView.h
+//  JNSelectionView
+//
+//  Created by admin on 2019/3/13.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoreSelectionView : UIView
+
+@end
---
+++ JNSelectionView/StoreSelectionView.m
@@ -0,0 +1,21 @@
+//
+//  StoreSelectionView.m
+//  JNSelectionView
+//
+//  Created by admin on 2019/3/13.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import "StoreSelectionView.h"
+
+@implementation StoreSelectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
